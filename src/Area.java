d67d070ea63712a290af170a8d9e713f56b7bc95
Shanukrshukla <shanushukla398@gmail.com>
2024-05-11 12:26:30 +0530

    init commit

---
+++ src/Area.java
@@ -0,0 +1,12 @@
+public class Area {
+    public static int calculateArea(int length, int width) {
+        return length * width;
+    }
+
+    public static void main(String[] args) {
+        int length = 5;
+        int width = 3;
+        int area = calculateArea(length, width);
+        System.out.println("Area of the rectangle: " + area);
+    }
+}
